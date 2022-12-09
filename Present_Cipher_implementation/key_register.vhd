c46d2866da710cb4999cac7f9d183a9378e43333
Kwame <odurokwameee@gmail.com>
2022-12-09 05:14:42 +0000

    keysheduler components

---
+++ Present_Cipher_implementation/key_register.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    04:11:13 12/09/2022 
+-- Design Name: 
+-- Module Name:    key_register - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity key_register is
+end key_register;
+
+architecture Behavioral of key_register is
+
+begin
+
+
+end Behavioral;
+
---
+++ Present_Cipher_implementation/sBox.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    03:52:46 12/09/2022 
+-- Design Name: 
+-- Module Name:    sBox - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity sBox is
+end sBox;
+
+architecture Behavioral of sBox is
+
+begin
+
+
+end Behavioral;
+
